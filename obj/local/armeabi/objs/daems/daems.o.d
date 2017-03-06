d5cf9c6db271e9688fcb1443db4aede2de7dddf4
mr-ooops <mr-ooops@gmx.com>
2017-03-07 05:32:02 +0800

    不计工时
    1、切换台式机环境
    2、记录闪退异常
    3、修正联系人布局细节

--- obj/local/armeabi/objs/daems/daems.o.d
+++ obj/local/armeabi/objs/daems/daems.o.d
@@ -1,13 +1,13 @@
 obj/local/armeabi/objs/daems/daems.o: jni/daems.cpp \
- F:/adt-bundle-windows-x86_64-20140624/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/jni.h \
- F:/adt-bundle-windows-x86_64-20140624/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/sys/cdefs.h \
- F:/adt-bundle-windows-x86_64-20140624/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/sys/cdefs_elf.h \
- F:/adt-bundle-windows-x86_64-20140624/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/android/api-level.h
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/jni.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/sys/cdefs.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/android/api-level.h
 
-F:/adt-bundle-windows-x86_64-20140624/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/jni.h:
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/jni.h:
 
-F:/adt-bundle-windows-x86_64-20140624/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/sys/cdefs.h:
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/sys/cdefs.h:
 
-F:/adt-bundle-windows-x86_64-20140624/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/sys/cdefs_elf.h:
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/sys/cdefs_elf.h:
 
-F:/adt-bundle-windows-x86_64-20140624/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/android/api-level.h:
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10b/platforms/android-L/arch-arm/usr/include/android/api-level.h:
